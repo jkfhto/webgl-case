023211e39d2e31e590804386ba6f09f2872c1758
DESKTOP-7NA3P8R\GLTD <470778561@qq.com>
2018-01-26 09:57:32 +0800

    no message

---
+++ files.js
@@ -0,0 +1,134 @@
+var files = {
+	"webgl": [
+		"树",
+		"地球",
+	],
+	"webgl / advanced": [
+		"webgl_buffergeometry",
+		"webgl_buffergeometry_constructed_from_geometry",
+		"webgl_buffergeometry_custom_attributes_particles",
+		"webgl_buffergeometry_drawcalls",
+		"webgl_buffergeometry_instancing",
+		"webgl_buffergeometry_instancing2",
+		"webgl_buffergeometry_instancing_billboards",
+		"webgl_buffergeometry_instancing_dynamic",
+		"webgl_buffergeometry_instancing_interleaved_dynamic",
+		"webgl_buffergeometry_lines",
+		"webgl_buffergeometry_lines_indexed",
+		"webgl_buffergeometry_points",
+		"webgl_buffergeometry_points_interleaved",
+		"webgl_buffergeometry_rawshader",
+		"webgl_buffergeometry_selective_draw",
+		"webgl_buffergeometry_uint",
+		"webgl_custom_attributes",
+		"webgl_custom_attributes_lines",
+		"webgl_custom_attributes_points",
+		"webgl_custom_attributes_points2",
+		"webgl_custom_attributes_points3",
+		"webgl_materials_modified",
+		"webgl_raymarching_reflect",
+		"webgl_shadowmap_pcss",
+		"webgl_simple_gi"
+	],
+	"webgl deferred": [
+		"webgldeferred_animation"
+	],
+	/*
+	"webgl2": [
+		"webgl2_sandbox"
+	],
+	*/
+	"webaudio": [
+		"webaudio_sandbox",
+		"webaudio_timing",
+		"webaudio_visualizer"
+	],
+	"webvr": [
+		"webvr_cubes",
+		"webvr_daydream",
+		"webvr_gearvr",
+		"webvr_panorama",
+		"webvr_rollercoaster",
+		"webvr_sandbox",
+		"webvr_video",
+		"webvr_vive",
+		"webvr_vive_dragging",
+		"webvr_vive_camerarig",
+		"webvr_vive_paint",
+		"webvr_vive_sculpt"
+	],
+	"misc": [
+		"misc_animation_authoring",
+		"misc_animation_keys",
+		"misc_controls_deviceorientation",
+		"misc_controls_fly",
+		"misc_controls_orbit",
+		"misc_controls_pointerlock",
+		"misc_controls_trackball",
+		"misc_controls_transform",
+		"misc_exporter_gltf",
+		"misc_exporter_obj",
+		"misc_fps",
+		"misc_lights_test",
+		"misc_lookat",
+		"misc_ubiquity_test",
+		"misc_ubiquity_test2",
+		"misc_uv_tests"
+	],
+	"css3d": [
+		"css3d_molecules",
+		"css3d_panorama",
+		"css3d_panorama_deviceorientation",
+		"css3d_periodictable",
+		"css3d_sandbox",
+		"css3d_sprites",
+		"css3d_youtube"
+	],
+	"canvas": [
+		"canvas_ascii_effect",
+		"canvas_camera_orthographic",
+		"canvas_geometry_birds",
+		"canvas_geometry_cube",
+		"canvas_geometry_earth",
+		"canvas_geometry_hierarchy",
+		"canvas_geometry_nurbs",
+		"canvas_geometry_panorama",
+		"canvas_geometry_panorama_fisheye",
+		"canvas_geometry_shapes",
+		"canvas_geometry_terrain",
+		"canvas_geometry_text",
+		"canvas_interactive_cubes",
+		"canvas_interactive_cubes_tween",
+		"canvas_interactive_particles",
+		"canvas_interactive_voxelpainter",
+		"canvas_lights_pointlights",
+		"canvas_lines",
+		"canvas_lines_colors",
+		"canvas_lines_colors_2d",
+		"canvas_lines_dashed",
+		"canvas_lines_sphere",
+		"canvas_materials",
+		"canvas_materials_normal",
+		"canvas_materials_reflection",
+		"canvas_materials_video",
+		"canvas_morphtargets_horse",
+		"canvas_particles_floor",
+		"canvas_particles_random",
+		"canvas_particles_sprites",
+		"canvas_particles_waves",
+		"canvas_performance",
+		"canvas_sandbox"
+	],
+	"raytracing": [
+		"raytracing_sandbox"
+	],
+	"software": [
+		"software_geometry_earth",
+		"software_lines_splines",
+		"software_sandbox"
+	],
+	"svg": [
+		"svg_lines",
+		"svg_sandbox"
+	]
+};
