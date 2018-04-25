a130bdacfb8f6574cf801ea186ffc6b9ecab2575
DESKTOP-C16P34B\jkf <470778561@qq.com>
2018-04-25 14:00:40 +0800

    no message

--- files.js
+++ files.js
@@ -1,9 +1,9 @@
 var files = {
 	"webgl": [
 		"树_tree/index",
-		"地球_earth/index",
+		"地球_earth/indexnew",
 		"星空_galaxy/index",
-		"管道_pipeline/indexnew",
+		"管道_pipeline/index",
 		"汽车展示_car/index",
 		"Shadertoy_Shadertoy/index",
 		"国家选中高亮_Country-Select-Highlight/index"
